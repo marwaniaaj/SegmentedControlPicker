56af24bfb9e48b64916b77c275687a28ff3b585f
Marwa <marwa.niaaj@gmail.com>
2024-01-01 14:08:50 +0400

    Initial Commit

---
+++ SegmentedControlPicker/SegmentedControlPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlPickerApp.swift
+//  SegmentedControlPicker
+//
+//  Created by Marwa Abou Niaaj on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
